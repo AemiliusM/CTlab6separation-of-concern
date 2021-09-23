c7296d3aa033ceb552c5981186e61abeae64691a
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-22 18:11:11 -0700

    failing patch test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,5 @@
 const pool = require('../lib/utils/pool');
-const twilio = require('twilio');
+// const twilio = require('twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
@@ -24,6 +24,54 @@ describe('03_separation-of-concerns-demo routes', () => {
         expect(res.body).toEqual({
           id: '1',
           quantity: 10
+        });
+      });
+  });
+
+  // sending post request with a quantity and use the get method to grab all the info from the tables
+  //  
+  it('gets all orders from /', async() => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+    return await request(app)
+      .get('/api/v1/orders')
+      .then(res => {
+        expect(res.body).toEqual([{
+          id:'1',
+          quantity: 10
+        }]);
+      });
+  });
+
+  it('gets orders from /id', async() => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ id:'1', quantity: 10 });
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ id:'2', quantity: 6 });
+    return await request(app)
+      .get('/api/v1/orders/1')
+      .then(res => {
+        expect(res.body).toEqual({
+          id:'1',
+          quantity: 10
+        });
+      });
+  });
+
+  it('updates an order from /id', async() => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ id:'1', quantity: 10 });
+    return await request(app)
+      .patch('/api/v1/orders/1')
+      .send({ id:'1', quantity: 1000 })
+      .then(res => {
+        expect(res.body).toEqual({
+          id:'1',
+          quantity: 1000
         });
       });
   });
